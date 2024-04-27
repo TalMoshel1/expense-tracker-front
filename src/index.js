fa685b0e06c95d960758693965a048e15658620f
unknown <talmoshel444@gmail.com>
2024-04-27 21:39:53 +0300

    tring to communicate with server-static site

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import App from './App';
 
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-
+import SigninRegister from '../src/pages/signIn-Register.js'
 import Header from './components/elements/Header';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Header />
-    <App />
+    {/* <Header />
+    <App /> */}
+    <SigninRegister/>
   </Provider>,
   document.getElementById('root')
 );
